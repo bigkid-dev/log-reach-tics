f8b9d6b3f6f3c18ff69d0aba1055448446eb911c
Witler <85369599+bigkid-dev@users.noreply.github.com>
2023-11-10 02:06:00 +0100

    Update settings.py

--- bmovez/settings.py
+++ bmovez/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*z^h(vyh3c)c*hibvn9u)2#c!)uy#3j-c%fx*(bm2r^eb5dnov'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
